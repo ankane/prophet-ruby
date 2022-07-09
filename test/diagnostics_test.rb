5483f537a873757a0de4d4101dbf85b36af10e90
Andrew Kane <andrew@ankane.org>
2022-07-09 17:09:18 -0400

    Improved diagnostics test

--- test/diagnostics_test.rb
+++ test/diagnostics_test.rb
@@ -9,16 +9,24 @@ class DiagnosticsTest < Minitest::Test
 
     df_cv = Prophet::Diagnostics.cross_validation(m, initial: "730 days", period: "180 days", horizon: "365 days")
     assert_equal 3988, df_cv.size
-    assert_times ["2010-02-15 00:00:00 UTC"], df_cv["cutoff"].first
-    assert_times ["2015-01-20 00:00:00 UTC"], df_cv["cutoff"].last
+    assert_times ["2010-02-16 00:00:00 UTC", "2010-02-17 00:00:00 UTC"], df_cv["ds"].head(2)
+    assert_times ["2016-01-19 00:00:00 UTC", "2016-01-20 00:00:00 UTC"], df_cv["ds"].tail(2)
+    assert_elements_in_delta [8.959074, 8.725548], df_cv["yhat"].head(2), 0.001
+    assert_elements_in_delta [9.068809, 8.905042], df_cv["yhat"].tail(2), 0.001
+    assert_elements_in_delta [8.242493, 8.008033], df_cv["y"].head(2), 0.001
+    assert_elements_in_delta [9.125871, 8.891374], df_cv["y"].tail(2), 0.001
+    assert_times ["2010-02-15 00:00:00 UTC", "2010-02-15 00:00:00 UTC"], df_cv["cutoff"].head(2)
+    assert_times ["2015-01-20 00:00:00 UTC", "2015-01-20 00:00:00 UTC"], df_cv["cutoff"].tail(2)
 
-    df_p = Prophet::Diagnostics.performance_metrics(df_cv, metrics: ["mse"])
+    df_p = Prophet::Diagnostics.performance_metrics(df_cv, metrics: ["mse", "rmse"])
     assert_equal 329, df_p.size
-    assert_equal 37, df_p["horizon"][0] / 86400
-    assert_equal 365, df_p["horizon"][-1] / 86400
+    assert_equal [37, 38], (df_p["horizon"].head(2) / 86400.0).to_a
+    assert_equal [364, 365], (df_p["horizon"].tail(2) / 86400.0).to_a
     # TODO fix
-    # assert_in_delta 0.494752, df_p["mse"][0]
-    # assert_in_delta 1.188329, df_p["mse"][-1]
+    # assert_elements_in_delta [0.494752, 0.500521], df_p["mse"].head(2), 0.001
+    # assert_elements_in_delta [1.175513, 1.188329], df_p["mse"].tail(2), 0.001
+    # assert_elements_in_delta [0.703386, 0.707475], df_p["rmse"].head(2), 0.001
+    # assert_elements_in_delta [1.084211, 1.090105], df_p["rmse"].tail(2), 0.001
   end
 
   def test_cross_validation_cutoffs
