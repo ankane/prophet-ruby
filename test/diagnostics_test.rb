f76559a133c4eeb3ba336c1d4c6c95c1f11efc6b
Andrew Kane <andrew@ankane.org>
2023-04-11 06:04:42 -0700

    Improved test [skip ci]

--- test/diagnostics_test.rb
+++ test/diagnostics_test.rb
@@ -32,7 +32,9 @@ class DiagnosticsTest < Minitest::Test
     # assert_elements_in_delta [0.703386, 0.707475], df_p["rmse"].head(2), 0.001
     # assert_elements_in_delta [1.084211, 1.090105], df_p["rmse"].tail(2), 0.001
 
-    Prophet::Plot.plot_cross_validation_metric(df_cv, metric: "mape").savefig("/tmp/cross_validation_mape.png")
+    if test_python?
+      Prophet::Plot.plot_cross_validation_metric(df_cv, metric: "mape").savefig("/tmp/cross_validation_mape.png")
+    end
   end
 
   def test_cross_validation_cutoffs
